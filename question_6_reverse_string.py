cba087c2eb3a4324e03d386efdc6d6a3a2f62769
Kenny TZY 123 (KENNY TAN ZI YANG) <80947205+Kenny-Tan-Zi-Yang@users.noreply.github.com>
2024-10-11 08:49:48 +0800

    Update question_6_reverse_string.py

--- question_6_reverse_string.py
+++ question_6_reverse_string.py
@@ -1,27 +1,25 @@
-# This code contains errors that you need to fix it. Read carefully and do necessary corrections.
-# Run the code at online-python.com or IDLE PYTHON before you commit the changes at github.com
-
 # This program is used to reverse a string.
 
 def string_reverse(str1):
     rstr1 = ''
     
     # Calculate the length of the input string 'str1'
-    index = length(str1) 
+    index = len(str1) - 1
     
     # Execute a while loop 
-    while index > 1:
+    while index >= 0:  # Corrected condition to include the first character
         # Concatenate the character of 'str1' to 'rstr1'
-        rstr1 -= str1[index + 1]
+        rstr1 += str1[index]  # Corrected operator and indexing
         
         # Decrement the 'index' by 1 for the next iteration
-        index = index + 1
+        index -= 1  # Corrected decrementing
     
     # Return the reversed string stored in 'rstr1'
-    return str1
+    return rstr1  # Return the reversed string
 
 def main():    
-    print("String reverse for \'12345abcde\' is", string_reverse(str1))
+    str1 = '12345abcde'  # Define the string to reverse
+    print("String reverse for \'{}\' is {}".format(str1, string_reverse(str1)))  # Pass str1 to the function
 
 # Don't change the code below!
 if __name__ == "__main__":
